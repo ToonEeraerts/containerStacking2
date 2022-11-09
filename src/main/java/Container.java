3e33daaab3f8382df1e52f1ac196e18385bb7a0c
Toon <eeraerts.toon@telenet.be>
2022-11-09 16:10:24 +0100

    Rdm

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -23,7 +23,7 @@ public class Container {
         return length;
     }
 
-    public void addSlots(Slot slot) {
-        this.slots.add(slot);
+    public void addSlot(Slot slot) {
+        slots.add(slot);
     }
 }
